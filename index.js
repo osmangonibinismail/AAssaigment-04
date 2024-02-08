81b856a015181bf5a9ac70bb5ea308b1496faf03
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-02-08 20:25:42 +0600

    finally updated

--- index.js
+++ index.js
@@ -1,68 +1,85 @@
-
-// function calculateMoney(ticketSale) {
+function calculateMoney(ticketSale) {
     
-//     if (ticketSale < 0 || typeof ticketSale !== 'number') {
-//         return 'Invalid Number';
-//     }
-//     const perTicketPrice = 120; 
-//     const securitySalary = 500;
-//     const lunchCost = 50;
-//     const worker = 8;
-//     const totalLunchCost = lunchCost * worker;
-//     const totalSell = perTicketPrice * ticketSale;
-//     const totalCost = securitySalary + totalLunchCost;
+    if (ticketSale < 0 || typeof ticketSale !== 'number') {
+        return 'Invalid Number';
+    }
+    const perTicketPrice = 120; 
+    const securitySalary = 500;
+    const lunchCost = 50;
+    const worker = 8;
+    const totalLunchCost = lunchCost * worker;
+    const totalSell = perTicketPrice * ticketSale;
+    const totalCost = securitySalary + totalLunchCost;
 
-//     return  totalSell - totalCost;
-// }
+    return  totalSell - totalCost;
+}
 
 
 
-// function checkName(name) {
-//     if (typeof name !== 'string'){
-//         return 'invalid';
-//     }
+function checkName(name) {
+    if (typeof name !== 'string'){
+        return 'invalid';
+    }
 
-//     const character = ['A', 'y', 'i', 'e', 'o', 'y', 'w'];
+    const character = ['A', 'y', 'i', 'e', 'o', 'y', 'w'];
 
-//     const toLowerCase = name.toLowerCase();
-//     const characterLowerCase = toLowerCase.substr(-1);
-//     if(character.includes(characterLowerCase)){
-//         return 'Good Name';
-//     }
-//     return 'Bad Name';
-// }
+    const toLowerCase = name.toLowerCase();
+    const characterLowerCase = toLowerCase.substr(-1);
+    if(character.includes(characterLowerCase)){
+        return 'Good Name';
+    }
+    return 'Bad Name';
+}
 
-// function deleteInvalids(array) {
-//     if(!Array.isArray(array)){
-//         return 'Invalid input'
-//     }
-//     let numbers = [];
-//     for (const arr of array) {
-//         if(typeof arr === 'number' && !isNaN(arr)){
-//             numbers.push(arr);
-//         }
-//     }
-//     return numbers;
-// }
+function deleteInvalids(array) {
+    if(!Array.isArray(array)){
+        return 'Invalid input'
+    }
+    let numbers = [];
+    for (const arr of array) {
+        if(typeof arr === 'number' && !isNaN(arr)){
+            numbers.push(arr);
+        }
+    }
+    return numbers;
+}
 
-// function password(obj) {
-//     const{name, birthYear, siteName} = obj;
+function password(obj) {
+    const{name, birthYear, siteName} = obj;
 
-//     if(typeof birthYear !== 'number' || Object.keys(obj).length !== 3 ){
-//         return 'invalid';
-//     }
-//     const firstElement = name.trim();
-//     const secondElement = birthYear.toString();
-//     const thirdElement = siteName.trim().charAt(0).toUpperCase() + siteName.slice(1).toLowerCase();
-//     if(secondElement.length !== 4){
-//         return 'invalid';
-//     }
-//     return thirdElement + "#" + firstElement + "@" + secondElement;
-// }
+    if(typeof birthYear !== 'number' || Object.keys(obj).length !== 3 ){
+        return 'invalid';
+    }
+    const firstElement = name.trim();
+    const secondElement = birthYear.toString();
+    const thirdElement = siteName.trim().charAt(0).toUpperCase() + siteName.slice(1).toLowerCase();
+    if(secondElement.length !== 4){
+        return 'invalid';
+    }
+    return thirdElement + "#" + firstElement + "@" + secondElement;
+}
 
-funtion monthlySavings(totalPayment, livingCost) {
-    if(Array.isArray !== totalPayments, livingCost) {
+function monthlySavings(totalPayment, livingCost) {
+    if(!Array.isArray === (totalPayment) || typeof livingCost !=='number') {
         return 'invalid input';
     }
+    let payment = 0;
+    for (const element of totalPayment) {
+        payment = payment + element
+    }
+    const totalIncome = payment;
+    let taxes = 0;
+    for (const element of totalPayment) {
+        let tax = 0;
+        if (element>=3000) {
+            tax = element*(20/100);
+        }
+        taxes = taxes + tax;
+    }
+    const totalTax = taxes;
+    const savings = totalIncome - (totalTax + livingCost);
+    if (savings<0) {
+      return 'earn more'  
+    }
+    return savings;
 }
-console.log(monthlySaving([ 1000 , 2000 , 3000 ] , 5400));
